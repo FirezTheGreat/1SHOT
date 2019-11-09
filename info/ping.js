35c4feedfbcf5bd1c416253b4596805978ceca5c
FirezzzZ <55920870+FirezzzZ@users.noreply.github.com>
2019-11-09 16:42:06 +0530

    Create ping.js

---
+++ info/ping.js
@@ -0,0 +1,12 @@
+  module.exports = {
+      config: {
+          name: "ping",
+          category: "info",
+          usage: " ",
+          description: "Returns latency and API ping",
+      },
+      run: async (client, message, args) => {
+          const msg = await message.channel.send(`🏓 Pinging....`);
+          msg.edit(`🏓 Pong!\nLatency is ${Math.floor(msg.createdTimestap - message.createdTimestap)}ms\nAPI Latency is ${Math.round(client.ping)}ms`);
+      }
+  }
