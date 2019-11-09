b3f78c65f38bd66f8d01f8f99b62389897e99823
FirezzzZ <55920870+FirezzzZ@users.noreply.github.com>
2019-11-09 16:35:01 +0530

    Create message.js

---
+++ events/guild/message.js
@@ -0,0 +1,20 @@
+const prefix = "!";
+const active = new Map();
+module.exports = async (bot, message) => { 
+    if(message.author.bot || message.channel.type === "dm") return;
+
+    let args = message.content.slice(prefix.length).trim().split(/ +/g);
+    let cmd = args.shift().toLowerCase();
+
+    if(!message.content.startsWith(prefix)) return;
+    try{
+        let ops = {
+            active: active
+        }
+    
+    var commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd))
+    if(commandfile) commandfile.run(bot, message, args, ops)
+    } catch (e) {
+        console.log(e);
+    }
+}
