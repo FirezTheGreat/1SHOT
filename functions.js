e878ce462655631578a6992868d07fa631be77ec
FirezzzZ <55920870+FirezzzZ@users.noreply.github.com>
2019-11-09 16:25:17 +0530

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,38 @@
+module.exports = {
+    getMember(message, toFind = '') {
+        toFind = toFind.toLowerCase();
+
+        var target = message.guild.members.get(toFind);
+
+        if(!target && message.mentions.members)
+        target = message.mentions.members.first();
+
+        if(!target && toFind) {
+            target = message.guild.members.find(member => {
+                return member.displayName.toLowerCase().includes(toFind) ||
+                member.user.tag.toLowerCase().includes(toFind)
+            });
+        }
+
+        if(!target)
+            target = message.member;
+
+            return target;
+    },
+
+    formatDate: function(date) {
+        return new Intl.DateTimeFormat('en-US').format(date);
+    },
+
+    promptMessage: async function(message, author, time, validReactions) {
+        time *= 1000;
+
+        for (const reaction of validReactions) await message.react(reaction);
+
+        const filter = (reaction, user) => validReactions.includes(reaction.emoji.name) && user.id === author.id;
+
+        return message
+            .awaitReactions(filter, { max: 1, time: time})
+            .then(collected => collected.first() && collected.first().emoji.name);
+    }
+}
