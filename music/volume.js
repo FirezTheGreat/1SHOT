1f2675724bc751ab246973f1b5f3289607be04cc
FirezzzZ <55920870+FirezzzZ@users.noreply.github.com>
2019-11-10 19:39:34 +0530

    Create volume.js

---
+++ music/volume.js
@@ -0,0 +1,9 @@
+module.exports = {
+    config: {
+        name: 'volume',
+        category: 'music',
+        description: 'Sets Volume',
+        usage: ' ',
+        accessableby: 'everyone'
+    }
+};
