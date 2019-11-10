6bd583988f31217f4d8efec3521892cd85c71920
FirezzzZ <55920870+FirezzzZ@users.noreply.github.com>
2019-11-10 19:38:34 +0530

    Create resume.js

---
+++ music/resume.js
@@ -0,0 +1,9 @@
+module.exports = {
+    config: {
+        name: 'resume',
+        category: 'music',
+        description: 'Resumes Music',
+        usage: ' ',
+        accessableby: 'everyone'
+    }
+};
