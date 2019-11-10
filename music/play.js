9035ff513c94ea8fdbda65abfa799efb852b91f1
FirezzzZ <55920870+FirezzzZ@users.noreply.github.com>
2019-11-10 19:35:08 +0530

    Create play.js

---
+++ music/play.js
@@ -0,0 +1,9 @@
+module.exports = {
+    config: {
+        name: 'play',
+        category: 'music',
+        description: 'Plays Music',
+        usage: ' ',
+        accessableby: 'everyone'
+    }
+}
