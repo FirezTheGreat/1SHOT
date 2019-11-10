a9e8bfbcdef11e290e5761351a526ca1db6708f3
FirezzzZ <55920870+FirezzzZ@users.noreply.github.com>
2019-11-10 19:39:04 +0530

    Create skip.js

---
+++ music/skip.js
@@ -0,0 +1,9 @@
+module.exports = {
+    config: {
+        name: 'skip',
+        category: 'music',
+        description: 'Skips music',
+        usage: ' ',
+        accessableby: 'everyone'
+    }
+};
