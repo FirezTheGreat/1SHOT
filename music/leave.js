472fba5787b87b2c154a9c3104e4f6738d839b01
FirezzzZ <55920870+FirezzzZ@users.noreply.github.com>
2019-11-10 19:35:43 +0530

    Create leave.js

---
+++ music/leave.js
@@ -0,0 +1,9 @@
+module.exports = {
+    config: {
+        name: 'leave',
+        category: 'music',
+        description: 'leaves the voice channel',
+        usage: ' ',
+        accessableby: 'everyone'
+    }
+};
