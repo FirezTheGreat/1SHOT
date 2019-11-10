8394db23edf8b58c71a121229ad157cb7b2cef3a
FirezzzZ <55920870+FirezzzZ@users.noreply.github.com>
2019-11-10 19:36:22 +0530

    create np.js

---
+++ music/np.js
@@ -0,0 +1,9 @@
+module.exports = {
+    config: {
+        name: 'np',
+        category: 'music',
+        description: 'Shows the music currently playing',
+        usage: ' ',
+        accessableby: 'everyone'
+    }
+};
