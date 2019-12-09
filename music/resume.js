f38c6c04ba66622e0a2f7ec4474d16d21079d167
FirezzzZ <55920870+FirezzzZ@users.noreply.github.com>
2019-12-09 19:05:32 +0530

    Delete resume.js

--- music/resume.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    config: {
-        name: 'resume',
-        category: 'music',
-        description: 'Resumes Music',
-        usage: ' ',
-        accessableby: 'everyone'
-    }
-};
