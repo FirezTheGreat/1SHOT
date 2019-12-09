142512bfc2e977fc87846faaaf19e99ed751691f
FirezzzZ <55920870+FirezzzZ@users.noreply.github.com>
2019-12-09 19:05:12 +0530

    Delete play.js

--- music/play.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    config: {
-        name: 'play',
-        category: 'music',
-        description: 'Plays Music',
-        usage: ' ',
-        accessableby: 'everyone'
-    }
-}
