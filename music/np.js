dcd433a1aa03511611067f4f00faf4df51aab6e7
FirezzzZ <55920870+FirezzzZ@users.noreply.github.com>
2019-12-09 19:04:52 +0530

    Delete np.js

--- music/np.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    config: {
-        name: 'np',
-        category: 'music',
-        description: 'Shows the music currently playing',
-        usage: ' ',
-        accessableby: 'everyone'
-    }
-};
