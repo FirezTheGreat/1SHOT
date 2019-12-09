bf64d36a3a51a4f823cab2f70a13f2e274da3dff
FirezzzZ <55920870+FirezzzZ@users.noreply.github.com>
2019-12-09 19:05:52 +0530

    Delete volume.js

--- music/volume.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    config: {
-        name: 'volume',
-        category: 'music',
-        description: 'Sets Volume',
-        usage: ' ',
-        accessableby: 'everyone'
-    }
-};
